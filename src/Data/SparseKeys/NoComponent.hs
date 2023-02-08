b468183231af70d4eac2472426f75cb0c93b282b
Simre1 <simre4775@gmail.com>
2023-02-08 17:52:36 +0100

    Add Sparse Keys and some Tests

---
+++ src/Data/SparseKeys/NoComponent.hs
@@ -0,0 +1,137 @@
+module Data.SparseKeys.NoComponent
+  ( SparseKeysNoComponent,
+    create,
+    nextKey,
+    contains,
+    size,
+    remove,
+    for,
+    visualize,
+    growDense,
+  )
+where
+
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (MonadIO (..))
+import Data.Bits
+import Data.IORef
+  ( IORef,
+    atomicModifyIORef,
+    atomicModifyIORef',
+    newIORef,
+    readIORef,
+    writeIORef,
+  )
+import Data.Kind (Constraint)
+import Data.Vector.Primitive qualified as VP
+import Data.Vector.Primitive.Mutable qualified as VPM
+import Data.Word (Word32)
+import Prelude hiding (lookup)
+
+-- | The Sparse keys are used to create keys with `nextKey`. The keys can be iterated over with `for`.
+-- Keys are created in constant time until the sparse keys set is completely full.
+data SparseKeysNoComponent = SparseKeysNoComponent
+  { sparseSetSparse :: {-# UNPACK #-} !(VPM.IOVector Word32),
+    sparseSetEntities :: {-# UNPACK #-} !(VPM.IOVector Word32),
+    sparseSetSize :: {-# UNPACK #-} !(IORef Int),
+    sparseSetHeadAndTail :: {-# UNPACK #-} !(IORef (Int, Int))
+  }
+
+-- | Creates a sparse set with the first value as the sparse array size and the second as the dense array size.
+-- Given that the sparse array size is x, then keys from 0..x are used. maxBound may never be used for x.
+-- Given that the dense array size is y, then y values can be stored. y should not be larger than x.
+create :: (MonadIO m) => Word32 -> Word32 -> m SparseKeysNoComponent
+create sparseSize denseSize = liftIO $ do
+  !sparse <- VPM.generate (fromIntegral sparseSize) (\i -> setBit (fromIntegral (succ i)) 31)
+  VPM.write sparse (fromIntegral sparseSize - 1) (setBit 0 31)
+  !entities <- VPM.new (fromIntegral denseSize)
+  let !size = 0
+  SparseKeysNoComponent sparse entities <$> newIORef size <*> newIORef (1, 0)
+{-# INLINE create #-}
+
+-- | Adds a new key to the set and returns it
+nextKey :: (MonadIO m) => SparseKeysNoComponent -> m Word32
+nextKey (SparseKeysNoComponent sparse entities sizeRef headAndTailRef) = liftIO $ do
+  (head, tail) <- readIORef headAndTailRef
+
+  nextIndex <- atomicModifyIORef' sizeRef (\size -> (succ size, size))
+  nextHead <- VPM.read sparse head
+
+  VPM.write sparse tail nextHead
+
+  VPM.unsafeWrite entities nextIndex (fromIntegral head)
+
+  VPM.unsafeWrite sparse head (fromIntegral nextIndex)
+
+  let nextHead' = fromIntegral $ clearBit nextHead 31
+
+  if nextHead' == head
+    then writeIORef headAndTailRef (-1, -1)
+    else writeIORef headAndTailRef (nextHead', tail)
+  pure (fromIntegral head)
+{-# INLINE nextKey #-}
+
+-- | Returns true if the given key is in the set.
+contains :: MonadIO m => SparseKeysNoComponent -> Word32 -> m Bool
+contains (SparseKeysNoComponent sparse _ _ _) i = liftIO $ do
+  v <- VPM.unsafeRead sparse (fromIntegral i)
+  pure $ not $ testBit v 31
+{-# INLINE contains #-}
+
+-- | Returns the amount of values in the set
+size :: MonadIO m => SparseKeysNoComponent -> m Int
+size (SparseKeysNoComponent _ _ sizeRef _) = liftIO $ readIORef sizeRef
+{-# INLINE size #-}
+
+-- | Removes an index from the set. Does nothing if the index does not exist.
+remove :: (MonadIO m) => SparseKeysNoComponent -> Word32 -> m ()
+remove (SparseKeysNoComponent sparse entities sizeRef headAndTailRef) i = liftIO $ do
+  index <- VPM.unsafeRead sparse (fromIntegral i)
+  if testBit index 31
+    then pure ()
+    else do
+      (head, tail) <- readIORef headAndTailRef
+      lastDenseIndex <- atomicModifyIORef sizeRef $ \size -> let s = max 0 (pred size) in (s, s)
+
+      lastKey <- VPM.unsafeRead entities lastDenseIndex
+      VPM.unsafeWrite entities (fromIntegral index) lastKey
+      VPM.unsafeWrite sparse (fromIntegral lastKey) index
+      if head /= -1
+        then do
+          VPM.unsafeWrite sparse tail (setBit i 31)
+          VPM.unsafeWrite sparse (fromIntegral i) (setBit (fromIntegral head) 31)
+          writeIORef headAndTailRef (head, fromIntegral i)
+        else do
+          VPM.unsafeWrite sparse (fromIntegral i) (setBit i 31)
+          writeIORef headAndTailRef (fromIntegral i, fromIntegral i)
+{-# INLINE remove #-}
+
+-- | Iterate over all values with their corresponding key.
+for :: (MonadIO m) => SparseKeysNoComponent -> (Word32 -> m ()) -> m ()
+for (SparseKeysNoComponent _ entities sizeRef _) f = do
+  size <- liftIO $ readIORef sizeRef
+
+  forM_ [0 .. pred size] $ \i -> do
+    key <- liftIO $ VPM.unsafeRead entities i
+
+    f key
+{-# INLINE for #-}
+
+-- | Grows the dense array by 50 percent.
+growDense :: (MonadIO m) => SparseKeysNoComponent -> m SparseKeysNoComponent
+growDense (SparseKeysNoComponent sparse entities sizeRef headAndTailRef) = liftIO $ do
+  let entitySize = VPM.length entities
+  newEntities <- VPM.unsafeGrow entities (entitySize `quot` 2)
+  pure $ SparseKeysNoComponent sparse newEntities sizeRef headAndTailRef
+
+-- | Visualizes the sparse set in the terminal. Mostly for debugging purposes.
+visualize :: MonadIO m => SparseKeysNoComponent -> m ()
+visualize (SparseKeysNoComponent sparse entities sizeRef headAndTailRef) = liftIO $ do
+  size <- readIORef sizeRef
+  (head, tail) <- readIORef headAndTailRef
+  putStrLn $ "SparseSet (" <> show size <> ")"
+  putStrLn $ "Head/Tail: " <> show head <> "/" <> show tail
+  putStr "Sparse: "
+  VP.freeze sparse >>= print
+  putStr "Dense: "
+  VP.freeze entities >>= print
--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,69 @@
 module Main (main) where
 
-import Data.SparseSet.Boxed qualified as Boxed
+import Data.SparseKeys.NoComponent qualified as K
+import Data.SparseSet.Storable qualified as S
+import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain sparseSet
+
+sparseSet :: TestTree
+sparseSet =
+  testGroup
+    "SparseSet"
+    [ testGroup
+        "Storable"
+        [ testCase "Insert/Lookup/Remove" $ do
+            set <- S.create @Int 5 5
+            S.size set >>= (@?= 0)
+            S.insert set 0 10
+            S.insert set 1 11
+            S.insert set 2 12
+            S.lookup set 0 >>= (@?= Just 10)
+            S.lookup set 1 >>= (@?= Just 11)
+            S.lookup set 2 >>= (@?= Just 12)
+            S.lookup set 3 >>= (@?= Nothing)
+            S.contains set 1 >>= (@?= True)
+            S.size set >>= (@?= 3)
+            S.remove set 1
+            S.size set >>= (@?= 2)
+            S.lookup set 1 >>= (@?= Nothing)
+            S.lookup set 0 >>= (@?= Just 10)
+            S.lookup set 2 >>= (@?= Just 12)
+            S.remove set 0
+            S.remove set 2
+            S.size set >>= (@?= 0)
+            S.contains set 0 >>= (@?= False)
+            S.contains set 2 >>= (@?= False)
+            (pure () :: IO ())
+        ],
+      testGroup
+        "NextKey"
+        [ testCase "NextKey/Remove" $ do
+            set <- K.create 2 2
+            K.size set >>= (@?= 0)
+            e <- K.nextKey set
+            K.size set >>= (@?= 1)
+            K.contains set e >>= (@?= True)
+            K.remove set e
+            K.size set >>= (@?= 0)
+            K.contains set e >>= (@?= False)
+            e1 <- K.nextKey set
+            e2 <- K.nextKey set
+            K.size set >>= (@?= 2)
+            K.contains set e1 >>= (@?= True)
+            K.contains set e2 >>= (@?= True)
+            (e1 /= e2) @? "e1 and e2 should not be the same"
+            K.remove set e1
+            e3 <- K.nextKey set
+            (e1 == e3) @? "e1 and e3 should be the same"
+            K.remove set e2
+            K.contains set e3 >>= (@?= True)
+            K.contains set e2 >>= (@?= False)
+            K.remove set e3
+            K.nextKey set
+            K.nextKey set
+            pure ()
+        ]
+    ]
